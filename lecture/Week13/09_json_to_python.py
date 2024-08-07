114e99ce9056a1be89d35cc608ec87a48b282b0b
Edwin Sumalabe Jr. <edwin.sumalabejr@benilde.edu.ph>
2024-08-07 20:01:03 +0800

    Update 09_json_to_python.py

--- lecture/Week13/09_json_to_python.py
+++ lecture/Week13/09_json_to_python.py
@@ -3,4 +3,6 @@ import json
 json_data = '{"name": "Zophie", "isCat": true, "mmiceCaught": "0", "felineIQ": null}'
 print(json_data)
 
-python_dict = json.loads()
+python_dict = json.loads(json_data)
+
+print(f"Name: {python_dict}")
