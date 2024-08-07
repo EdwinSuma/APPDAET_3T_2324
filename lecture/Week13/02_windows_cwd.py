2990dc8adde90d0c4008f5d955d6c64e00d8051d
Edwin Sumalabe Jr. <edwin.sumalabejr@benilde.edu.ph>
2024-08-07 18:48:03 +0800

    Week 13
    added lecture files

---
+++ lecture/Week13/02_windows_cwd.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+import os
+
+print(Path.cwd()) # cwd = Current Working Directory
+
+os.chdir("C:\\Windows\\System32")
+print(Path.cwd())
+
---
+++ lecture/Week13/07_writing_txt_files_w.py
@@ -0,0 +1,5 @@
+ulam_file = open("Lunch.txt", "w") # open a file for writing
+
+ulam_file.write("Sinigang na Baboy\n") # write a line of text to a file
+
+ulam_file.close() # close the file
